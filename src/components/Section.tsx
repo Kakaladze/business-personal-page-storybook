151b321f63e1921fec069390e0fd8f423a6f64e7
Krzysztof Szybowicz <szybowicz.krzysztof@gmail.com>
2026-04-07 15:15:42 +0200

    Initial Version of section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,40 +1,102 @@
 import styled from "styled-components";
+import { TextHeader } from "./TextHeader";
+import { Box } from "./Containers";
 
-const Body = styled.div({
-      background: "#020617",
+const SectionTest = styled.section(({ theme }) => ({
+  position: 'relative',
+  background: 'transparent',
+  padding: theme.space.md,
+  overflow: 'hidden',
+  width: '100%',
+  minHeight: '450px',
+  boxSizing: 'border-box'
+}))
+
+const Content = styled.div({
+  position: 'relative',
+  zIndex: '2'
 })
 
-const SectionTest = styled.section({
-  position: "relative",
-  padding: "100px 0",
-  overflow: "hidden",
-  height: '300px',
-  color: "white",
+const BottomShape = styled.section(() => ({
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  width: "100%",
+  lineHeight: 0,
+  zIndex: 0,
+  pointerEvents: 'none',
+  
+  "& svg": {
+    width: '100%',
+    height: '120px',
+    display: 'block',
+    zIndex: 0,
+    transform: 'translateZ(0)', 
+    willChange: 'filter', 
+    overflow: 'visible !important'
+  }, 
+}))
 
-    "&::after": {
-    content: '""',
-    position: "absolute",
-    left: "0%",
-    top: "20%",
-    transform: 'rotate(-2deg)',
-    // transform: "translate(-50%, 50%)",
+interface SectionProps {
+  headerText: string,
+  pageBackground: string,
+  children: React.ReactNode
+}
 
-    width: "100%",
-    height: "50%",
-    // borderRadius: "40%",
-    borderBottomRightRadius: '30%',
-    borderBottomLeftRadius: '50%',
+export const Section = ({ headerText, pageBackground, children }: SectionProps) => (
+    <>
+    <SectionTest>
+        <Content>
+            <TextHeader headerText={headerText} paddingX="2xl" paddingY="sm" marginBottom="md" />
+            <Box paddingX="2xl">
+              {children}
+            </Box>
+        </Content>
 
-    background: "#FFFFFF14", // 👈 tylko kolor
-    filter: "blur(20px)",    // 👈 robi glow
+        <BottomShape>
+          <svg viewBox="0 0 1440 120" preserveAspectRatio="none">
+            <defs>
+              <filter
+                id="shadow"
+                filterUnits="userSpaceOnUse"
+                x="-200"
+                y="-300"
+                width="1800"
+                height="600"
+              >
+                <feDropShadow 
+                  dx="0" 
+                  dy="-60" 
+                  stdDeviation="60" 
+                  floodColor="rgb(164,163,175, 0.6)" 
+                />
+              </filter>
+            </defs>
 
-    pointerEvents: "none",
-    }
-});
-
-export const Section = () => (
-    <Body>
-        <SectionTest>Moje Umiejętności</SectionTest>
-        <SectionTest>Wybrane Projekty</SectionTest>
-    </Body>
+            <path
+              d="
+                M0,80
+                C400,80 900,160 1440,80
+                L1440,120
+                L0,120
+                Z
+              "
+              fill={pageBackground}
+              filter="url(#shadow)"
+            />
+            
+            <path
+              d="
+                M0,80
+                C400,80 900,160 1440,80
+                L1440,120
+                L0,120
+                Z
+              "
+              fill={pageBackground}
+            />
+          </svg>
+        </BottomShape>
+    </SectionTest>
+    </>
 )
--- src/stories/Section.stories.tsx
+++ src/stories/Section.stories.tsx
@@ -1,10 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Section } from '../components/Section';
+import { Text } from '../components/Text';
 
 const meta = {
   title: 'Components/Section',
   component: Section,
   tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div style={{ background: '#0b1220' }}>
+        <Story />
+                <Story />
+        <Story />
+
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof Section>;
 
 export default meta;
@@ -14,5 +25,8 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
+    headerText: 'Hello World',
+    children: <Text> Hello </Text>,
+    pageBackground: "#0b1220"
   }
 };
